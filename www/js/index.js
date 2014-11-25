54804a69c71f143bc4b5025c83be0e9ccb45e51f
hancoma <hancoma@gmail.com>
2014-11-25 13:15:03 +0900

    upload_file 앱에서 처리
    키캣 문제로 앱에서 파일 업로드 처리

--- www/js/index.js
+++ www/js/index.js
@@ -52,6 +52,16 @@ var ref = window.open('http://m.rococophoto.net', '_blank', 'location=no');
 ref.addEventListener('loadstart', function(event) { 
          navigator.notification.activityStart("RococoPhoto", "loading");
 
+   // 링크 주소 확인
+        var link=event.url;
+        var result=link.indexOf('upload_file.php');
+        alert(result);
+        // 파일 업로드 
+        if(result>-1) {
+            getImage();
+        }
+
+        
     
     });
     ref.addEventListener('loadstop', function(event) { 
@@ -65,3 +75,36 @@ ref.addEventListener('loadstart', function(event) {
 
     }
 };
+
+function getImage() {
+        // Retrieve image file location from specified source
+        navigator.camera.getPicture(uploadPhoto, function(message) {
+alert('get picture failed');
+},{
+quality: 50,
+destinationType: navigator.camera.DestinationType.FILE_URI,
+sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
+});}
+    function uploadPhoto(imageURI) {
+        var options = new FileUploadOptions();
+        options.fileKey="file";
+        options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
+        options.mimeType="image/jpeg";
+
+        var params = new Object();
+        params.value1 = "test";
+        params.value2 = "param";
+
+        options.params = params;
+        options.chunkedMode = false;
+
+        var ft = new FileTransfer();
+        ft.upload(imageURI, "http://m.rococophoto.net/upload.php", win, fail, options);
+    }
+
+    function win(r) {
+        console.log("Code = " + r.responseCode);
+        console.log("Response = " + r.response);
+        console.log("Sent = " + r.bytesSent);
+        alert(r.response);
+    }
