0818be5a173f43861e5db482be8df4f6556f18d8
hancoma <hancoma@gmail.com>
2014-11-25 14:09:25 +0900

    컴파일 파일 삭제
    컴파일 파일 삭제

--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\ch7_000\Documents\GitHub\rococophoto_new\platforms\android\ant-build\AndroidManifest.xml : \
-C:\Users\ch7_000\Documents\GitHub\rococophoto_new\platforms\android\AndroidManifest.xml \
--- platforms/android/ant-build/rococophoto-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\Users\ch7_000\Documents\GitHub\rococophoto_new\platforms\android\ant-build\rococophoto-debug-unaligned.apk : \
-C:\Users\ch7_000\Documents\GitHub\rococophoto_new\platforms\android\ant-build\rococophoto.ap_ \
-C:\Users\ch7_000\Documents\GitHub\rococophoto_new\platforms\android\ant-build\classes.dex \
-C:\Users\ch7_000\Documents\GitHub\rococophoto_new\platforms\android\CordovaLib\ant-build\classes.jar \
