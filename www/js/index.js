0ea5b464fe6bae92c103268066b0833378a45ffe
hancoma <hancoma@gmail.com>
2014-11-26 15:01:44 +0900

    웹뷰로 수정
    웹뷰로 나오도록 수정

--- www/js/index.js
+++ www/js/index.js
@@ -52,7 +52,7 @@ var app = {
 
          navigator.notification.activityStart("RococoPhoto", "loading");
          var link_home='http://m.rococophoto.net/?uuid='+uuid;
-var ref = window.open(link_home, '_system', 'location=no');
+var ref = window.open(link_home, '_blank', 'location=no');
 ref.addEventListener('loadstart', function(event) { 
          navigator.notification.activityStart("RococoPhoto", "loading");
 
