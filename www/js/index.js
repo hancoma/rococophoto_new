bba969530aba2a7cef151a76767a1dd23b22dd34
hancoma <hancoma@gmail.com>
2014-11-29 02:55:04 +0900

    뒤로가기

--- www/js/index.js
+++ www/js/index.js
@@ -57,7 +57,7 @@ var app = {
 
          navigator.notification.activityStart("RococoPhoto", "loading");
          var link_home='http://m.rococophoto.net/?uuid='+uuid;
-var ref = window.open(link_home, '_self', 'location=no');
+var ref = window.open(link_home, '_blank', 'location=no');
 ref.addEventListener('loadstart', function(event) { 
          navigator.notification.activityStart("RococoPhoto", "loading");
 
