2d15120bb9808b4f94613a1cccc735926a169947
hancoma <hancoma@gmail.com>
2014-11-29 19:43:34 +0900

    뒤로가기 처리하기
    뒤로가기 처리 하기 -

--- www/js/index.js
+++ www/js/index.js
@@ -70,7 +70,7 @@ if (link_home) {
 } else {
     var link_home='http://m.rococophoto.net/?uuid='+uuid;
 }
-var ref = window.open(link_home, '_blank', 'location=yes');
+var ref = window.open(link_home, '_blank', 'location=no');
 ref.addEventListener('loadstart', function(event) { 
          navigator.notification.activityStart("RococoPhoto", "loading");
 
