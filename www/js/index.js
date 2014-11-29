4d62b5679f087ced015e3e13306558eff2518dcf
hancoma <hancoma@gmail.com>
2014-11-29 22:29:57 +0900

    뒤로가기 처리하기
    뒤로가기 처리하기

--- www/js/index.js
+++ www/js/index.js
@@ -56,12 +56,12 @@ if (historyUrl.length >= 1)
 var link_home= historyUrl.pop(); // 이번 pop이 기존 url이다.
 } else{
 
-         var link_home='http://m.rococophoto.net/?c=0&uuid='+uuid;
+         var link_home='http://m.rococophoto.net/index.php?uuid='+uuid;
 }
 if (link_home) {
 
 } else {
-    var link_home='http://m.rococophoto.net/?c=0';
+    var link_home='http://m.rococophoto.net/index.php';
 }
 
  navigator.notification.activityStart("RococoPhoto", "loading");
